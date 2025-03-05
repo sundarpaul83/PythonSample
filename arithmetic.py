0cb1be3314953cbe2b0a0c19c9293e76dde50232
sundarpaul83 <pkskpaulsamy@gmail.com>
2025-03-05 22:55:46 +0530

    created  arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,13 @@
+a = 46  # Initializing the value of a
+b = 4  # Initializing the value of b
+
+print("For a =", a, "and b =", b, "\nCalculate the following:")
+
+# printing different results
+print("1. Addition of two numbers: a + b =", a + b)
+print("2. Subtraction of two numbers: a - b =", a - b)
+print("3. Multiplication of two numbers: a * b =", a * b)
+print("4. Division of two numbers: a / b =", a / b)
+print("5. Floor division of two numbers: a // b =", a // b)
+print("6. Reminder of two numbers: a mod b =", a % b)
+print("7. Exponent of two numbers: a ^ b =", a**b)
